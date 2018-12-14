4d8c2f19475e19f613e9729849599c453a1ac0d9
Carlos AP <carlos.agullo@gmail.com>
2018-12-13 23:51:22 -0500

    First tests

--- d04/lib/guard.rb
+++ d04/lib/guard.rb
@@ -1,3 +1,13 @@
 class Guard
 
+  def sleeping
+    50
+  end
+
 end
+
+# data = File.read('../data.txt')
+
+@g = Guard.new()
+
+@g.sleeping
--- d04/spec/guard_spec.rb
+++ d04/spec/guard_spec.rb
@@ -1,5 +1,25 @@
 require "guard"
 
 describe Guard do
+  before do
+    @g = Guard.new()
+    @guard10 =  %q(
+      [1518-11-01 00:00] Guard #10 begins shift
+      [1518-11-01 00:05] falls asleep
+      [1518-11-01 00:25] wakes up
+      [1518-11-01 00:30] falls asleep
+      [1518-11-01 00:55] wakes up'
+    )
+    
+    @guard99 = %q(
+    [1518-11-01 23:58] Guard #99 begins shift
+    [1518-11-02 00:40] falls asleep
+    [1518-11-02 00:50] wakes up
+    )
+  end
+
+  describe '.sleep' do
+    expect(@guard10.sleeping).to eq(50)
+  end
 
 end
