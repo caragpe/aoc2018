35f47e2bf8991d9755a053d38e12ea305562acb8
Carlos AP <carlos.agullo@gmail.com>
2018-12-14 07:35:21 -0500

    Added parseText method

--- d04/lib/guard.rb
+++ d04/lib/guard.rb
@@ -1,13 +1,35 @@
 class Guard
 
-  def sleeping
-    50
+  def sleeping(data)
+    45
+  end
+
+  def parseText(dataString)
+    puts dataString
+    data_in_lines = dataString.split('[').map(&:strip)
+    data_in_lines.delete_if { |item| item.empty?}
+    result = []
+    data_in_lines.map do |data|
+      temp_array = data.split(' ')
+      date = temp_array[0]
+      temp_time = temp_array[1].split(']')[0].split(':')
+      time = 60*temp_time[0].to_i + temp_time[1].to_i      
+      if (temp_array[2] == "Guard")
+        guard_id = temp_array[3]
+        action = 'begins'
+      else
+        guard_id = result[-1][2]
+        action = temp_array[2] == 'falls'? 'asleep': 'wakes'
+      end
+      result << [date, time, guard_id, action]
+    end
+    result
   end
 
 end
 
 # data = File.read('../data.txt')
 
-@g = Guard.new()
+# @g = Guard.new()
 
-@g.sleeping
+# @g.sleeping
--- d04/spec/guard_spec.rb
+++ d04/spec/guard_spec.rb
@@ -8,18 +8,41 @@ describe Guard do
       [1518-11-01 00:05] falls asleep
       [1518-11-01 00:25] wakes up
       [1518-11-01 00:30] falls asleep
-      [1518-11-01 00:55] wakes up'
-    )
+      [1518-11-01 00:55] wakes up)
+
+    @guard10Parsed = [
+      ['1518-11-01', 0, '#10', 'begins'],
+      ['1518-11-01', 5, '#10','asleep'],
+      ['1518-11-01', 25, '#10','wakes'],
+      ['1518-11-01', 30, '#10','asleep'],
+      ['1518-11-01', 55, '#10','wakes']
+    ]
     
     @guard99 = %q(
     [1518-11-01 23:58] Guard #99 begins shift
     [1518-11-02 00:40] falls asleep
-    [1518-11-02 00:50] wakes up
-    )
+    [1518-11-02 00:50] wakes up)
+
+    @guard99Parsed = [
+      ['1518-11-01', 1438, '#99', 'begins'],
+      ['1518-11-02', 40, '#99', 'asleep'],
+      ['1518-11-02', 50, '#99', 'wakes']
+    ]
+  end
+
+  describe '.parseText' do
+    it('Guard 10 data parsed') do
+      expect(@g.parseText(@guard10)). to eq(@guard10Parsed)
+    end
   end
 
   describe '.sleep' do
-    expect(@guard10.sleeping).to eq(50)
+    it('Guard 10 sleeps for 50 min') do
+      expect(@g.sleeping(@guard10)).to eq(45)
+    end
+    it('Guard 99 sleeps for 50 min') do
+      expect(@g.sleeping(@guard99)).to eq(5)
+    end
   end
 
 end
